55247a6dd6ecce5d07724acfdecb59369d00d0a5
CentoSquare <contact@centosquare.com>
2022-01-01 13:45:37 +0500

    cron job for stormarknad katrineholm jul

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -29,7 +29,7 @@ class Kernel extends ConsoleKernel
             ->daily();
 
         $schedule->command('stormarknadkatrineholmjul:cron')
-            ->everyMinute();
+                ->hourlyAt(10);
     }
 
     /**
